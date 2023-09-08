333d8fcdfd8f65246c26f289876b1525389b0a6c
marantonieto <marcioantoni16@gmail.com>
2023-09-07 22:14:21 -0300

    FEAT-Kruskal-algorithm-implemented

--- graph.py
+++ graph.py
@@ -13,6 +13,38 @@ class Graph:
     def create_edge(self, edge):
         self.edges.append(edge)
 
+    def find_root(self, parent, node):
+        if parent[node] == node:
+            return node
+        return self.find_root(parent, parent[node])
+
+    def join(self, parent, rank, u, v):
+        lord_a = self.find_root(parent, u)
+        lord_b = self.find_root(parent, v)
+        if lord_a != lord_b:
+            if rank[lord_a] < rank[lord_b]:
+                parent[lord_a] = lord_b
+            elif rank[lord_a] > rank[lord_b]:
+                parent[lord_b] = lord_a
+            else:
+                parent[lord_b] = lord_a
+                rank[lord_a] += 1
+
+    def kruskal(self):
+        mst_edges = []
+        self.edges.sort(key=lambda edge: edge.distance)
+
+        parent = {node: node for node in self.nodes}
+        rank = {node: 0 for node in self.nodes}
+
+        for edge in self.edges:
+            u, v = edge.start, edge.end
+            if self.find_root(parent, u) != self.find_root(parent, v):
+                mst_edges.append(edge)
+                self.join(parent, rank, u, v)
+
+        return mst_edges
+
     def print_graph(self):
         printed_edges = set()
         for i in self.nodes:
--- main.py
+++ main.py
@@ -2,44 +2,99 @@ from edges import Edge
 from graph import Graph
 from nodes import Node
 
-amount_nodes = int(input("How much nodes do you want in graph? "))
+# amount_nodes = int(input("How much nodes do you want in graph? "))
+#
+# # Graph creation with specified quantity of nodes (departments) in context of this application.
+# main_graph = Graph()
+#
+# print('')
+# print("All right, now specify the Departments (Nodes): ")
+# print('')
+#
+# for i in range(amount_nodes):
+#     name = input(f'department {i+1} name: ')
+#     amount_people = int(input(f'department {i + 1} peoples: '))
+#     new_node = Node(name, amount_people)
+#     main_graph.create_node(new_node)
+#
+# print(f'All right, now, specify the edges. max{amount_nodes - 1}')
+#
+# while True:
+#     start = input("Start department name: ")
+#     final = input("Final Department name: ")
+#     distance = input("Distance between them: ")
+#
+#     node_start = 1
+#     node_final = 1
+#
+#     for nodes in main_graph.nodes:
+#         if start == nodes.department_name:
+#             node_start = nodes
+#     for nodes in main_graph.nodes:
+#         if final == nodes.department_name:
+#             node_final = nodes
+#
+#     created_edge = Edge(node_start, node_final, distance)
+#     main_graph.create_edge(created_edge)
+#
+#     conf = input('Do you want to create any connections? (y/n) ')
+#     if conf != "y":
+#         break
+#
+# print("")
+# main_graph.print_graph()
 
-# Graph creation with specified quantity of nodes (departments) in context of this application.
 main_graph = Graph()
 
-print('')
-print("All right, now specify the Departments (Nodes): ")
-print('')
+node1 = Node("DC", 3000)
+node2 = Node("Ceagri I", 200)
+node3 = Node("Cegoe", 2500)
+node4 = Node("DB", 100)
+node5 = Node("Depaq", 200)
+node6 = Node("Reitoria", 2)
 
-for i in range(amount_nodes):
-    name = input(f'department {i+1} name: ')
-    amount_people = int(input(f'department {i + 1} peoples: '))
-    new_node = Node(name, amount_people)
-    main_graph.create_node(new_node)
+main_graph.create_node(node1)
+main_graph.create_node(node2)
+main_graph.create_node(node3)
+main_graph.create_node(node4)
+main_graph.create_node(node5)
+main_graph.create_node(node6)
 
-print(f'All right, now, specify the edges. max{amount_nodes - 1}')
+# edge1 = Edge(node1, node2, 15)
+# edge2 = Edge(node2, node3, 13)
+# edge3 = Edge(node1, node3, 20)
+# edge4 = Edge(node3, node5, 5)
+# edge5 = Edge(node5, node4, 8)
+# edge6 = Edge(node2, node4, 5)
+# edge7 = Edge(node4, node6, 15)
+# edge8 = Edge(node2, node6, 25)
+# edge9 = Edge(node6, node5, 21)
+# edge10 = Edge(node3, node4, 10)
 
-while True:
-    start = input("Start department name: ")
-    final = input("Final Department name: ")
-    distance = input("Distance between them: ")
+edge4 = Edge(node3, node5, 5)
+edge6 = Edge(node2, node4, 5)
+edge5 = Edge(node5, node4, 8)
+edge10 = Edge(node3, node4, 10)
+edge2 = Edge(node2, node3, 13)
+edge1 = Edge(node1, node2, 15)
+edge7 = Edge(node4, node6, 15)
+edge3 = Edge(node1, node3, 20)
+edge9 = Edge(node6, node5, 21)
+edge8 = Edge(node2, node6, 25)
 
-    node_start = 1
-    node_final = 1
 
-    for nodes in main_graph.nodes:
-        if start == nodes.department_name:
-            node_start = nodes
-    for nodes in main_graph.nodes:
-        if final == nodes.department_name:
-            node_final = nodes
+main_graph.create_edge(edge1)
+main_graph.create_edge(edge2)
+main_graph.create_edge(edge3)
+main_graph.create_edge(edge4)
+main_graph.create_edge(edge5)
+main_graph.create_edge(edge6)
+main_graph.create_edge(edge7)
+main_graph.create_edge(edge8)
+main_graph.create_edge(edge9)
+main_graph.create_edge(edge10)
 
-    created_edge = Edge(node_start, node_final, distance)
-    main_graph.create_edge(created_edge)
+minimum_spanning_tree = main_graph.kruskal()
+for edge in minimum_spanning_tree:
+    print(f"{edge.start.department_name} -> {edge.end.department_name} cost: {edge.distance}")
 
-    conf = input('Do you want to create any connections? (y/n) ')
-    if conf != "y":
-        break
-
-print("")
-main_graph.print_graph()
